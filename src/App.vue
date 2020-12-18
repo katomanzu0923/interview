949e2d0bbcc59858bba630aeae51ada429421324
katomanzu0923 <katomanzu86@gmail.com>
2020-12-18 14:54:44 +0900

    have revised code to make the good design

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,93 @@
 <template>
   <div class="appBody">
     <div>
-      <h2>面接練習へようこそ</h2>
+      <h2 :class="[ButtonSelect === 'new' ? 'NewTitle' : 'defaultTitle', ButtonSelect === 'old' ? 'OldTitle' : 'defaultTitle', ButtonSelect === 'tech'? 'TechTitle' : 'defaultTitle']">面接練習へようこそ</h2>
     </div>
-    <div class="button">
-      <button @click="start1()">開始</button>
+    <div class="PartButton">
+      <div v-if="NewNone" class="NewButton">
+        <button @click="PositionNew()">新卒として練習する</button>
+      </div>
+      <div v-if="OldNone" class="OldButton">
+        <button @click="PositionOld()">中途として練習する</button>
+      </div>
+      <div v-if="OldNone" class="TechButton">
+        <button @click="PositionTech()">技術質問として練習する</button>
+      </div>
     </div>
-    <div class="notification">今回の気づき<div>{{memo}}</div></div>
+    <div class="PartButton">
+      <div :class="[ButtonSelect === 'new' ? 'NewButton' : 'classB', ButtonSelect === 'old' ? 'OldButton' : 'classD', ButtonSelect === 'tech'? 'TechButton' : 'class']">
+        <button @click="start()" v-if="none">開始</button>
+      </div>
+      <div :class="[ButtonSelect === 'new' ? 'NewButton' : 'classB', ButtonSelect === 'old' ? 'OldButton' : 'classD', ButtonSelect === 'tech'? 'TechButton' : 'class']">
+        <button @click="back()" v-if="none">選択へ戻る</button>
+      </div>
+    </div>
+    <div :class="[ButtonSelect === 'new' ? 'NewNotification' : 'classB', ButtonSelect === 'old' ? 'OldNotification' : 'classD', ButtonSelect === 'tech'? 'TechNotification' : 'class']" v-if="none">今回の気づき</div>
+    <div class="memo" v-if="none">{{memo}}</div>
     <transition name="fade">
       <div v-if="isOpen == 'up'">
-        <Contents :select="select" @isOff="off" @protect="memo= $event">メモらん</Contents>
+        <NewContents :select="select" :part="part" @isOff="off" @protect="memo= $event">メモらん</NewContents>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-import Contents from "./components/Contents.vue";
+import NewContents from "./components/NewContents.vue";
 export default {
   components: {
-    Contents
+    NewContents
   },
   data() {
     return {
       foot: "0",
       isOpen:'down',
       select: 0,
-      memo:"特になし"
+      part:"new",
+      memo:"特になし",
+      none: false,
+      NewNone:true,
+      OldNone:true,
+      ButtonSelect: ''
     }
   },
   methods: {
-    start1() {
+    start() {
       this.isOpen = 'up'
       let number = 1 + Math.floor( Math.random() * 4 )
       this.select = number
       this.memo = "特になし"
     },
+    back() {
+      this.NewNone = true
+      this.OldNone = true
+      this.none = false
+      this.ButtonSelect = ''
+      this.memo = "特になし"
+    },
     off() {
       this.isOpen = 'down'
+    },
+    PositionNew() {
+      this.none = true
+      this.NewNone = false
+      this.OldNone = false
+      this.ButtonSelect = 'new'
+      this.part = 'new'
+    },
+    PositionOld() {
+      this.none = true
+      this.NewNone = false
+      this.OldNone = false
+      this.ButtonSelect = 'old'
+      this.part = 'old'
+    },
+    PositionTech() {
+      this.none = true
+      this.NewNone = false
+      this.OldNone = false
+      this.ButtonSelect = 'tech'
+      this.part = 'tech'
     }
   }
 };
@@ -47,11 +97,35 @@ export default {
 h2 {
   width: 100%;
   text-align: center;
-  color: red;
+  color:white;
+  border-bottom: 1px solid black;
+  width: 15%;
+  margin: 50px auto;
+  -webkit-text-stroke: 1px black;
+  text-stroke: 1px black;
 }
 button {
-  text-align: center;
-  margin: 0 auto;
+  padding: 20px;
+  text-decoration: none;
+  outline: none;
+}
+.NewButton button:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
+}
+.OldButton button:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
+}
+.TechButton button:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
 }
 .appBody {
   
@@ -68,10 +142,125 @@ button {
 .fade-enter-to {
 	opacity: 1;
 }
-.button {
+.defaultTitle {
+  width: 100%;
+  text-align: center;
+  color:white;
+  border-bottom: 1px solid black;
+  width: 15%;
+  margin: 50px auto;
+  -webkit-text-stroke: 1px black;
+  text-stroke: 1px black;
+}
+.NewTitle {
+  width: 100%;
+  text-align: center;
+  color:rgba(247, 205, 90, 0.548);
+  border-bottom: 1px solid red;
+  width: 15%;
+  margin: 50px auto;
+  -webkit-text-stroke: 1px red;
+  text-stroke: 1px red;
+}
+.OldTitle {
+  width: 100%;
+  text-align: center;
+  color:rgba(90, 247, 247, 0.548);
+  border-bottom: 1px solid blue;
+  width: 15%;
+  margin: 50px auto;
+  -webkit-text-stroke: 1px blue;
+  text-stroke: 1px blue;
+}
+.TechTitle {
+  width: 100%;
+  text-align: center;
+  color:white;
+  border-bottom: 1px solid green;
+  width: 15%;
+  margin: 50px auto;
+  -webkit-text-stroke: 1px green;
+  text-stroke: 1px green;
+}
+.PartButton {
+  display: flex;
+  justify-content: center;
+}
+.NewButton {
+  display: inline;
+  margin: 20px;
+  font-size: 1rem;
+}
+.NewButton button {
+  color: red;
+  background:rgba(247, 205, 90, 0.548);
+  border:1px solid rgb(255, 0, 0);
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(255, 0, 0, 0.822);
+}
+
+.OldButton {
+  display: inline;
+  margin: 20px;
+}
+.OldButton button {
+  color: blue;
+  background:rgba(90, 247, 247, 0.548);
+  border:1px solid blue;
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(25, 0, 255, 0.822);
+}
+.TechButton {
+  display: inline;
+  margin: 20px;
+}
+.TechButton button {
+  color: rgb(0, 128, 0);
+  background:rgba(148, 247, 90, 0.548);
+  border:1px solid green;
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(0, 128, 0, 0.822);
+}
+.NewNotification{
+  text-align: center;
+  color: red;
+  background:rgba(247, 205, 90, 0.548);
+  border:1px solid rgb(255, 0, 0);
+  border-radius: 20px;
+  width: 10%;
+  margin: 0 auto;
+}
+.OldNotification{
+  text-align: center;
+  color: blue;
+  background:rgba(90, 247, 247, 0.548);
+  border:1px solid blue;
+  border-radius: 20px;
+  width: 10%;
+  margin: 0 auto;
+}
+.TechNotification{
+  text-align: center;
+  color: green;
+  background:rgba(148, 247, 90, 0.548);
+  border:1px solid green;
+  border-radius: 20px;
+  width: 10%;
+  margin: 0 auto;
+}
+.memo {
   text-align: center;
 }
-.notification {
+
+.OldMemo {
+  text-align: center;
+  background: rgb(0, 60, 255);
+  margin: 0 auto;
+  border: 1px solid red;
+  border-radius: 20px;
+  width: 10%;
+  margin: 10px;
   text-align: center;
 }
+
 </style>
---
+++ src/components/NewContents.vue
@@ -0,0 +1,383 @@
+<template>
+	<div class="contentsBody" v-if="!off">
+		<div v-if="part =='new'">
+			<div class="contents" v-if="select=== 1">
+				<div class="NewTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">あなたの自己prを教えてください</p>
+				<p v-else-if="number === 3">その中で我が社に行かせそうなところを教えてください</p>
+				<p v-else-if="number === 4">弱みを教えてください</p>
+				<p v-else-if="number === 5">ありがとうございました。それでは何か質問はありませんか？</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="NewText">memo</textarea>
+				<img src="@/assets/face01.png" alt="">
+				</div>
+				<button @click="next()" class="NewButton">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select===  2">
+				<div class="NewTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">あなたが頑張ったことを教えてください</p>
+				<p v-else-if="number === 3">その中であなたが工夫したことを教えてください</p>
+				<p v-else-if="number === 4">今回のあなたの就活軸を教えてください</p>
+				<p v-else-if="number === 5">それを踏まえて弊社を志望する理由を教えてください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="NewText">memo</textarea>
+				<img src="@/assets/face02.png" alt="">
+				</div>
+				<button @click="next()" class="NewButton">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 3">
+				<div class="NewTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">あなたの自己PRを教えてください</p>
+				<p v-else-if="number === 3">なぜこの職種を希望されたのですか</p>
+				<p v-else-if="number === 4">それはなぜですか</p>
+				<p v-else-if="number === 5">あなたの失敗談を教えてください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="NewText">memo</textarea>
+				<img src="@/assets/face03.png" alt="">
+				</div>
+				<button class="NewButton" @click="next()" >{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 4">
+				<div class="NewTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">学生時代に力を入れてきたこと</p>
+				<p v-else-if="number === 3">なぜそのことに取り組もうと考えたのですか</p>
+				<p v-else-if="number === 4">現在、プログラミング学習 ではどのようなことを学習しているのですか</p>
+				<p v-else-if="number === 5">プログラミングを学んでみた感想</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="NewText">memo</textarea>
+				<img src="@/assets/face04.png" alt="">
+				</div>
+				<button class="NewButton" @click="next()" >{{down}}</button>
+			</div>
+			
+			<div>{{finish}}</div>
+		</div>
+		<div v-else-if="part =='old'">
+			<div class="contents" v-if="select=== 1">
+				<div class="OldTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">あなたの自己prを教えてください</p>
+				<p v-else-if="number === 3">その中で我が社に行かせそうなところを教えてください</p>
+				<p v-else-if="number === 4">弱みを教えてください</p>
+				<p v-else-if="number === 5">ありがとうございました。それでは何か質問はありませんか？</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="OldText">memo</textarea>
+				<img src="@/assets/face01.png" alt="">
+				</div>
+				<button class="OldButton" @click="next()" >{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select===  2">
+				<div class="OldTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてne</p>
+				<p v-else-if="number === 2">あなたが頑張ったことを教えてください</p>
+				<p v-else-if="number === 3">その中であなたが工夫したことを教えてください</p>
+				<p v-else-if="number === 4">今回のあなたの就活軸を教えてください</p>
+				<p v-else-if="number === 5">それを踏まえて弊社を志望する理由を教えてください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="OldText">memo</textarea>
+				<img src="@/assets/face03.png" alt="">
+				</div>
+				<button class="OldButton" @click="next()">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 3">
+				<div class="OldTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">あなたの自己PRを教えてください</p>
+				<p v-else-if="number === 3">なぜこの職種を希望されたのですか</p>
+				<p v-else-if="number === 4">それはなぜですか</p>
+				<p v-else-if="number === 5">あなたの失敗談を教えてください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="OldText">memo</textarea>
+				<img src="@/assets/face04.png" alt="">
+				</div>
+				<button class="OldButton" @click="next()">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 4">
+				<div class="OldTitle">
+					<p v-if="number=== 1">あなたの自己紹介を教えてください</p>
+				<p v-else-if="number === 2">学生時代に力を入れてきたこと</p>
+				<p v-else-if="number === 3">なぜそのことに取り組もうと考えたのですか</p>
+				<p v-else-if="number === 4">現在、プログラミング学習 ではどのようなことを学習しているのですか</p>
+				<p v-else-if="number === 5">プログラミングを学んでみた感想</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="OldText">memo</textarea>
+				<img src="@/assets/face02.png" alt="">
+				</div>
+				<button class="OldButton" @click="next()">{{down}}</button>
+			</div>
+			
+			<div>{{finish}}</div>
+		</div>
+		<div v-else>
+			<div class="contents" v-if="select=== 1">
+				<div class="TechTitle">
+					<p v-if="number=== 1">MVCの仕組みについて説明してください</p>
+				<p v-else-if="number === 2">AWSとは何ですか？また、AWSの裏側ではどのような技術が動いていますか？</p>
+				<p v-else-if="number === 3">Ruby の「include」とは何ですか？</p>
+				<p v-else-if="number === 4">エラー解決はどのように行いますか？</p>
+				<p v-else-if="number === 5">ログイン、ログアウトの仕組みを説明してください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="TechText">memo</textarea>
+				<img src="@/assets/face01.png" alt="">
+				</div>
+				<button class="TechButton" @click="next()">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select===  2">
+				<div class="TechTitle">
+					<p v-if="number=== 1">MVCの仕組みについて説明してください</p>
+				<p v-else-if="number === 2">AWSとは何ですか？また、AWSの裏側ではどのような技術が動いていますか？</p>
+				<p v-else-if="number === 3">Ruby の「include」とは何ですか？</p>
+				<p v-else-if="number === 4">エラー解決はどのように行いますか？</p>
+				<p v-else-if="number === 5">ログイン、ログアウトの仕組みを説明してください</p>
+				<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="TechText">memo</textarea>
+				<img src="@/assets/face02.png" alt="">
+				</div>
+				<button class="TechButton" @click="next()">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 3">
+				<div class="TechTitle">
+					<p v-if="number=== 1">MVCの仕組みについて説明してください</p>
+					<p v-else-if="number === 2">AWSとは何ですか？また、AWSの裏側ではどのような技術が動いていますか？</p>
+					<p v-else-if="number === 3">Ruby の「include」とは何ですか？</p>
+					<p v-else-if="number === 4">エラー解決はどのように行いますか？</p>
+					<p v-else-if="number === 5">ログイン、ログアウトの仕組みを説明してください</p>
+					<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="TechText">memo</textarea>
+				<img src="@/assets/face04.png" alt="">
+				</div>
+				<button class="TechButton" @click="next()">{{down}}</button>
+			</div>
+			<div class="contents" v-else-if="select=== 4">
+				<div>
+				<div class="TechTitle">
+					<p v-if="number=== 1" >デスクトップPCとスマートフォンでは画面のサイズが異なりますが、どのように対応しますか？</p>
+					<p v-else-if="number === 2">viewを作成するときにエンジニア目線で気をつけていることはありますか？</p>
+					<p v-else-if="number === 3">データベースのトランザクションについて説明してください</p>
+					<p v-else-if="number === 4">データベースのトランザクションについて説明してください</p>
+					<p v-else-if="number === 5">Rubyの長所、短所について教えてください</p>
+					<p v-else-if="number === 6">ありがとうございました。それでは何か質問はありませんか？</p>
+				</div>
+				<div class="flex">
+					<textarea name="" id="" cols="30" rows="10" v-model="memo" placeholder="気になったことをメモ" class="TechText">memo</textarea>
+				<img src="@/assets/face03.png" alt="">
+				</div>
+				<button class="TechButton" @click="next()">{{down}}</button>
+			</div>
+			</div>
+			
+			<div>{{finish}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			down: "次へ",
+			number:1,
+			memo:"",
+		}
+	},
+	props: {
+		select: {
+			type: Number,
+			default: 1
+		},
+		part: {
+			type: String,
+			default: "new"
+		}
+	},
+	methods: {
+		next() {
+			if (this.number >= 5) {
+				if (this.number == 5) {
+					this.down = "終了"
+					this.number = this.number +1 
+				}else if (this.number == 6) {
+					this.number = 0
+					this.$emit('isOff')
+				}
+			}else{
+				this.$emit('protect', this.memo)
+				this.number = this.number +1
+				this.down = "次へ"
+			}
+		},
+		
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+	width: 60%;
+	left: 25%;
+	height: 600px;
+}
+textarea {
+	width: 20%;
+	outline: none;
+}
+p {
+	font-size: 2rem;
+	text-align: center;
+	margin-bottom: 5%;
+	width: 100%;
+	}
+button {
+	outline: none;
+}
+button:active {
+  /*ボタンを押したとき*/
+  -webkit-transform: translateY(4px);
+  transform: translateY(4px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
+}
+.NewTitle p{
+  width: 100%;
+  text-align: center;
+  color:rgba(247, 205, 90, 0.548);
+  margin: 50px auto;
+  -webkit-text-stroke: 1px red;
+  text-stroke: 1px red;
+}
+.OldTitle p{
+  width: 100%;
+  text-align: center;
+  color:rgba(90, 247, 247, 0.548);
+  margin: 50px auto;
+  -webkit-text-stroke: 1px blue;
+  text-stroke: 1px blue;
+}
+.TechTitle p{
+  width: 100%;
+  text-align: center;
+  color:rgba(148, 247, 90, 0.548);
+  margin: 50px auto;
+  -webkit-text-stroke: 1px green;
+  text-stroke: 1px green;
+}
+.NewButton  {
+  color: red;
+  background:rgba(247, 205, 90, 0.548);
+  border:1px solid rgb(255, 0, 0);
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(255, 0, 0, 0.822);
+	width: 10%;
+	font-size: 1.5rem;
+	padding: 5px;
+	margin: 10px auto;
+}
+.OldButton {
+  color: blue;
+  background:rgba(90, 247, 247, 0.548);
+  border:1px solid blue;
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(25, 0, 255, 0.822);
+	width: 10%;
+	font-size: 1.5rem;
+	padding: 5px;
+	margin: 10px auto;
+}
+.TechButton {
+  color: rgb(0, 128, 0);
+  background:rgba(148, 247, 90, 0.548);
+  border:1px solid green;
+  border-radius: 20px;
+  border-bottom: solid 4px rgba(0, 128, 0, 0.822);
+	width: 10%;
+	font-size: 1.5rem;
+	padding: 5px;
+	margin: 10px auto;
+}
+.NewText {
+  color: red;
+  background:rgba(247, 205, 90, 0.548);
+  border:1px solid rgb(255, 0, 0);
+  border-radius: 20px;
+	width: 30%;
+	font-size: 1rem;
+	padding: 5px;
+	margin: 0 5px;
+}
+.OldText {
+  color: blue;
+  background:rgba(90, 247, 247, 0.548);
+  border:1px solid blue;
+  border-radius: 20px;
+	width: 30%;
+	font-size: 1rem;
+	padding: 5px;
+	margin: 0 5px;
+}
+.TechText {
+  color: rgb(0, 128, 0);
+  background:rgba(148, 247, 90, 0.548);
+  border:1px solid green;
+  border-radius: 20px;
+	width: 30%;
+	font-size: 1rem;
+	padding: 5px;
+	margin: 0 5px;
+}
+.contentsBody {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+
+}
+#button {
+	display: block;
+	text-align: center;
+	width: 20%;
+	margin-left: 40%;
+	margin-top: 5%;
+}
+.contents {
+	position: absolute;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	bottom: 10%;
+	text-align: center;
+	background: white;
+}
+.flex {
+	
+	display: flex;
+	justify-content: space-around;
+}
+.memo {
+}
+</style>
